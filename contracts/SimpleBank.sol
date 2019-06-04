d41211f210f067135940a842e258e1fdbfebc15e
Richard Garcia <richard@innervent.net>
2019-06-03 22:12:01 -0500

    updated contract to pass all tests

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,26 +13,27 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address indexed accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address indexed accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
+    event LogWithdrawal(address indexed accountAddress, uint newBalance,  uint withdrawAmount);
 
 
     //
@@ -42,20 +43,26 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
+        require(enrolled[msg.sender] == false);
+        enrolled[msg.sender] = true;
+        balances[msg.sender] = 0.0;
+        emit LogEnrolled(msg.sender);
     }
 
     /// @notice Deposit ether into bank
@@ -64,9 +71,17 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
-        /* Add the amount to the user's balance, call the event associated with a deposit,
+    function deposit() public payable returns (uint) {
+                /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        require(enrolled[msg.sender] == true);
+
+        uint currentBalance = getBalance(); //get current balance
+        balances[msg.sender] = (currentBalance + msg.value); //set new balance
+
+    
+        emit LogDepositMade(msg.sender, msg.value); //call event
+        return balances[msg.sender]; //return new address
     }
 
     /// @notice Withdraw ether from bank
@@ -75,6 +90,12 @@ contract SimpleBank {
     /// @return The balance remaining for the user
     // Emit the appropriate event    
     function withdraw(uint withdrawAmount) public returns (uint) {
+        require(balances[msg.sender] >= withdrawAmount);
+        uint currentBalance = getBalance();
+        balances[msg.sender] = currentBalance - withdrawAmount;
+        //send withdraw amount? 
+        emit LogWithdrawal(msg.sender, balances[msg.sender], withdrawAmount);
+        return balances[msg.sender];
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
